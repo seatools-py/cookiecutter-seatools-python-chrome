260eae2b555c9ee829e6430e0082f493d7511bf3
dragons96 <jinqunlong@black-unique.com>
2024-11-20 12:09:15 +0800

    release: 1.0.2

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitee.com/seatools-py/cookiecutter-seatools-python-chrome',
-    version='1.0.1',
+    version='1.0.2',
     zip_safe=False,
     entry_points={
         'console_scripts': [
